0f8a03833f69f1f8b5f060578a8ea87ac36323a2
srujan <srujanprophet@gmail.com>
2020-06-18 21:36:19 +0530

    Add stage 5. Add exit and show remaining resources of coffee machine.Add Error handling if not enough resources.Add go back option after selecting buy to return

--- 2. Coffee Machine/Source.java
+++ 2. Coffee Machine/Source.java
@@ -13,15 +13,45 @@ public class Source {
     }
 
     public static void buy() {
-    	System.out.println("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino:");
-        int choice = sc.nextInt();
-        if (choice == 1) {
+    	System.out.println("\nWhat do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino:");
+        String choice = sc.next();
+        if (choice.equals("back")) {
+        	System.out.println();
+        	return;
+        }
+        if (cups == 0) {
+        	System.out.println("Sorry, not enough cups!\n");
+        	return;
+        }
+        if (choice.equals("1")) {
+        	if (water < 250) {
+        		System.out.println("Sorry, not enough water!\n");
+        		return;
+        	}
+        	if (cbeans < 16) {
+        		System.out.println("Sorry, not enough coffee beans!\n");
+        		return;
+        	}
+        	System.out.println("I have enough resources, making you a coffee!\n");
 		    water -= 250;
 		    cbeans -= 16;
 		    money += 4;
 		    cups--;
 		}
-		else if (choice == 2) {
+		else if (choice.equals("2")) {
+			if (water < 350) {
+        		System.out.println("Sorry, not enough water!\n");
+        		return;
+        	}
+        	if (cbeans < 20) {
+        		System.out.println("Sorry, not enough coffee beans!\n");
+        		return;
+        	}
+        	if (milk < 75) {
+        		System.out.println("Sorry, not enough milk!\n");
+        		return;
+        	}
+        	System.out.println("I have enough resources, making you a coffee!\n");
 		    water -= 350;
 		    milk -= 75;
 		    cbeans -= 20;
@@ -29,14 +59,25 @@ public class Source {
 		    cups--;
 	    }
 	    else {
+	    	if (water < 200) {
+        		System.out.println("Sorry, not enough water!\n");
+        		return;
+        	}
+        	if (cbeans < 100) {
+        		System.out.println("Sorry, not enough coffee beans!\n");
+        		return;
+        	}
+        	if (milk < 12) {
+        		System.out.println("Sorry, not enough milk!\n");
+        		return;
+        	}
+        	System.out.println("I have enough resources, making you a coffee\n");
 		    water -= 200;
 		    milk -= 100;
 		    cbeans -= 12;
 		    money += 6;
 		    cups--;
 	    }
-	    System.out.println();
-	    stock(water, milk, cbeans, cups, money);
     }
 
     public static void fill() {
@@ -53,25 +94,31 @@ public class Source {
 		milk += mn;
 		cups += cn;
 		System.out.println();
-		stock(water, milk, cbeans, cups, money);
     }
 
     public static void take() {
-    	money = 0;
+    	System.out.println("I gave you $" + money);
     	System.out.println();
-		stock(water, milk, cbeans, cups, money);
+    	money = 0;
     }
 
     public static void main(String[] args) {
-        stock(water, milk, cbeans, cups, money);
-        System.out.println("Write action (buy, fill, take):");
+        System.out.println("Write action (buy, fill, take, remaining, exit):");
         String action = sc.next();
-        if (action.equals("buy")) {
-            buy();
-	    }
-	    else if (action.equals("fill")) {
-	    	fill();
+        while (!action.equals("exit")) {
+        	if (action.equals("buy")) {
+            	buy();
+	    	}
+	    	else if (action.equals("fill")) {
+	    		fill();
+	   		}
+	   		else if (action.equals("remaining")) {
+	   			System.out.println();
+				stock(water, milk, cbeans, cups, money);
+	   		}
+	   		else take();
+	   		System.out.println("Write action (buy, fill, take, remaining, exit):");
+        	action = sc.next();
 	   	}
-	   	else take();
     }
 }
