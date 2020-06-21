abed4cda2f508fbbc268724d50da31c588b45503
srujan <srujanprophet@gmail.com>
2020-06-21 19:30:14 +0530

    Add tic-tac-toe stage 2 code

--- 3. Tic-Tac-Toe/Source.java
+++ 3. Tic-Tac-Toe/Source.java
@@ -1,8 +1,15 @@
-public class Source {
+import java.util.Scanner;
+
+public class Source  {
     public static void main(String[] args) {
-        // write your code here
-        System.out.println("X O X");
-        System.out.println("O X O");
-        System.out.println("X X O");
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter cells : ");
+	String s = sc.next();
+        System.out.println("---------");
+        System.out.println("| " + s.charAt(0) + " " + s.charAt(1) + " " + s.charAt(2) + " |");
+        System.out.println("| " + s.charAt(3) + " " + s.charAt(4) + " " + s.charAt(5) + " |");
+        System.out.println("| " + s.charAt(6) + " " + s.charAt(7) + " " + s.charAt(8) + " |");
+        System.out.println("---------");
     }
 }
+
