fb08789387466913a520a494167090debf2b17c6
srujan <srujanprophet@gmail.com>
2020-06-21 19:21:38 +0530

    add project 3 - tic tac toe stage 1

---
+++ 3. Tic-Tac-Toe/Source.java
@@ -0,0 +1,8 @@
+public class Source {
+    public static void main(String[] args) {
+        // write your code here
+        System.out.println("X O X");
+        System.out.println("O X O");
+        System.out.println("X X O");
+    }
+}
