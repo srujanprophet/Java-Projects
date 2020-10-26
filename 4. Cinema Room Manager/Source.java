4baf687deafc52374ec4bfca7627234a558280d5
srujan <srujanprophet@gmail.com>
2020-10-26 20:20:40 +0530

    Finish Stage 3. Print seats, ask for seat, show chosen seat's price, print seats with selected seat as booked

--- 4. Cinema Room Manager/Source.java
+++ 4. Cinema Room Manager/Source.java
@@ -1,5 +1,32 @@
 import java.util.Scanner;
+
 public class Source {
+    
+    static void printRoom(int r, int s, int rno, int sno) {
+        int k;
+        System.out.println("\n" + "Cinema:");
+        for(int i = 0; i < r+1; i++) {
+            if(i == 0) {
+                System.out.print("  ");
+            }
+            else {
+                System.out.print(i + " ");
+            }
+            for(int j=1; j < s+1; j++) {
+                if(i==0) {
+                    System.out.print(j + " ");
+                }
+                else {
+                    if(rno == i && sno == j) 
+                        System.out.print("B ");
+                    else
+                        System.out.print("S ");
+                }
+            }
+            System.out.println("");
+        }
+        System.out.println();
+    } 
 
     public static void main(String[] args) {
         // Write your code here
@@ -8,16 +35,23 @@ public class Source {
         int r = sc.nextInt();
         System.out.println("Enter the number of seats in each row: ");
         int s = sc.nextInt();
-        System.out.println("Total Income: ");
+        printRoom(r,s,-1,-1);
+        System.out.println("Enter a row number: ");
+        int rno = sc.nextInt();
+        System.out.println("Enter a seat number in that row: ");
+        int sno = sc.nextInt();
         int total = r*s;
+        int price;
         if(total <= 60) { 
-            System.out.println("$"+(total*10));
+            price = 10;
         } 
         else {
             int fh = r/2;
             int bh = r-fh;
-            int cost = fh*10*s + bh*8*s;
-            System.out.println("$"+cost); 
+            if(rno <= fh) price = 10;
+            else price = 8;
         }
+        System.out.println("\nTicket Price: $"+price);
+        printRoom(r,s,rno,sno);
     }
 }
