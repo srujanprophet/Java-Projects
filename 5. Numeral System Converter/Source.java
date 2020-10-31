31a8998127a5c5dcfbb1e8fce88baef93423ef70
srujan <srujanprophet@gmail.com>
2020-10-31 21:16:44 +0530

    Finish till stage 5 of project 5, implement the fractional part of numeral system converter

--- 5. Numeral System Converter/Source.java
+++ 5. Numeral System Converter/Source.java
@@ -15,7 +15,30 @@ public class Source {
             System.out.println();
         }
         else {
-            System.out.println(Integer.toString(Integer.parseInt(n, src_radix), dest_radix));
+            StringBuilder before_dot = new StringBuilder();
+            StringBuilder after_dot = new StringBuilder();
+            int dot_hit = 0;
+            for(int i=0; i<n.length(); i++) {
+                if(n.charAt(i) == '.') dot_hit = 1;
+                else if(dot_hit == 0) before_dot.append(n.charAt(i));
+                else after_dot.append(n.charAt(i));
+            }
+            //System.out.println(before_dot + " " + after_dot);
+            String int_part = (Integer.toString(Integer.parseInt(before_dot.toString(), src_radix), dest_radix));
+            System.out.print(int_part);
+            if(after_dot.length() != 0) {
+                System.out.print(".");
+                double num = 0;
+                for(int i=0; i<after_dot.length(); i++) {
+                    num += (Integer.parseInt(String.valueOf(after_dot.charAt(i)), src_radix)) / Math.pow(src_radix,i+1);
+                }
+                for(int i=0; i < 5; i++) {
+                    double x = num * dest_radix;
+                    System.out.print(Integer.toString((int)x, dest_radix));
+                    num = x - (int)x;
+                }
+            }
+            System.out.println();
         }
     }
 }
