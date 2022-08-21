047d26fef7dbe8dc35b275ce4d292b11bf03a9e0
Srujan Patel <srujanprophet@gmail.com>
2022-08-21 22:28:50 +0530

    Create Main_4.java

---
+++ 9. Bulls and Cows/Code/Main_4.java
@@ -0,0 +1,69 @@
+// 4. Game Time
+
+package bullscows;
+
+import java.util.*;
+
+public class Main {
+
+    static Scanner sc = new Scanner(System.in);
+
+    static void get_cowBull(String guess, String code) {
+        int cows = 0, bulls = 0;
+        for (int i = 0; i < code.length(); i++) {
+            if (guess.charAt(i) == code.charAt(i)) {
+                bulls++;
+            }
+            else if (code.contains(String.valueOf(guess.charAt(i)))) {
+                cows++;
+            }
+        }
+        if (bulls == 0) {
+            System.out.printf("Grade: %d cow(s)\n", cows);
+        } else if (cows == 0) {
+            System.out.printf("Grade: %d bull(s)\n", bulls);
+        } else {
+            System.out.printf("Grade: %d bull(s) and %d cow(s)\n", bulls, cows);
+        }
+    }
+
+    static String getCode(int len) {
+        long pseudoRandomNumber = System.nanoTime();
+        StringBuilder code = new StringBuilder();
+        while (code.length() != len) {
+            Long digit = pseudoRandomNumber % 10;
+            int isUnique = code.indexOf(Long.toString(digit));
+            if ((code.length() == 0 && digit == 0) || isUnique != -1) {
+                pseudoRandomNumber /= 10;
+                if (pseudoRandomNumber == 0L) pseudoRandomNumber = System.nanoTime();
+                continue;
+            }
+            code.append(digit);
+            pseudoRandomNumber /= 10;
+            if (pseudoRandomNumber == 0L) pseudoRandomNumber = System.nanoTime();
+        }
+        return code.toString();
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Please, enter the secret code's length:");
+        int len = sc.nextInt();
+        int turn = 1;
+        System.out.println("Okay, let's start a game!");
+        if (len > 10) {
+            System.out.printf("Error! Can't generate a secret number with a " +
+                    "length of %d because there aren't enough unique digits.", len);
+            return;
+        }
+        String secret_code = getCode(len);
+        while (true) {
+            System.out.printf("Turn %d:\n", turn++);
+            String guess = sc.next();
+            get_cowBull(guess, secret_code);
+            if (guess.equals(secret_code)) {
+                System.out.println("Congratulations! You guessed the secret code.");
+                break;
+            }
+        }
+    }
+}
