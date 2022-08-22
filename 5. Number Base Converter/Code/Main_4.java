ecba2edc2c207196964a556f0598e6735281c8a2
Srujan Patel <srujanprophet@gmail.com>
2022-08-22 22:19:30 +0530

    Update and rename 5. Number Base Converter/Source.java to 5. Number Base Converter/Code/Main_4.java

--- 5. Number Base Converter/Code/Main_4.java
+++ 5. Number Base Converter/Code/Main_4.java
@@ -1,37 +1,22 @@
-package converter;
-import java.util.Scanner;
+// 4. Convert Fractions
 
-public class Source {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int src_radix,dest_radix;
-        String n;
-        try {
-            src_radix = sc.nextInt();
-            n = sc.next();
-            dest_radix = sc.nextInt();
-        } catch (Exception e) {
-            System.out.println("Error : Invalid input");
-            return;
-        }
-        // error handling
-        if (src_radix < 1 || src_radix > 36 || dest_radix < 1 || dest_radix > 36  ) {
-            System.out.println("Error : Radix not valid");
-            return;
-        }
-        if (n.length() == 0) {
-            System.out.println("Error : Invalid input");
-            return;
-        }
-        try {
+package converter;
+
+import java.util.Scanner;
+import java.math.BigInteger;
+
+public class Main {
+    
+   static void convert(int src_radix, int dest_radix, String n) {
+       try {
+            System.out.print("Conversion result: ");
             if (src_radix == 1) {
                 int dec = n.length();
                 System.out.println(Integer.toString(dec,dest_radix));
             } else if(dest_radix == 1) {
                 int dec = Integer.parseInt(n,src_radix);
                 for(int i=0; i < dec; i++) System.out.print("1");
-                System.out.println();
-            }
+            } else if (src_radix == dest_radix) System.out.print(n);
             else {
                 StringBuilder before_dot = new StringBuilder();
                 StringBuilder after_dot = new StringBuilder();
@@ -42,7 +27,7 @@ public class Source {
                     else after_dot.append(n.charAt(i));
                 }
                 //System.out.println(before_dot + " " + after_dot);
-                String int_part = (Integer.toString(Integer.parseInt(before_dot.toString(), src_radix), dest_radix));
+                String int_part = (Long.toString(Long.parseLong(before_dot.toString(), src_radix), dest_radix));
                 System.out.print(int_part);
                 if(after_dot.length() != 0) {
                     System.out.print(".");
@@ -56,12 +41,33 @@ public class Source {
                         num = x - (int)x;
                     }
                 }
-                System.out.println();
             }
         } catch (Exception e) {
             System.out.println("Error : Invalid conversions");
             return;
         }
+   }
+
+   public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        while (true) {
+            System.out.print("\nEnter two numbers in format: {source base} {target base} (To quit type /exit) ");
+            String in1 = sc.next();
+            if (in1.equals("/exit")) break;
+            int src_radix = Integer.parseInt(in1); 
+            int dest_radix = Integer.parseInt(sc.next());
+            // error handling
+            if (src_radix < 1 || src_radix > 36 || dest_radix < 1 || dest_radix > 36  ) {
+                System.out.println("Error : Radix not valid");
+                return;
+            }
+            while (true) {
+                System.out.printf("\nEnter number in base %d to convert to base %d (To go back type /back) ", src_radix, dest_radix);
+                String n = sc.next();
+                if (n.equals("/back")) break;
+                convert(src_radix, dest_radix, n);
+            }
+        }
     }
 }
-
