abc594ca03ed7b0705b5de8f3eea8c76c235f3c9
Srujan Patel <srujanprophet@gmail.com>
2022-08-22 12:40:18 +0530

    Create Main_3.java

---
+++ 1. Simple Chatty Bot/Code/Main_3.java
@@ -0,0 +1,36 @@
+// 3. Guess the Age
+
+package bot;
+
+import java.util.Scanner;
+
+public class SimpleBot {
+   final static Scanner scanner = new Scanner(System.in); // Do not change this line
+
+    public static void main(String[] args) {
+        greet("Reyna", "2020"); // change it as you need
+        remindName();
+        guessAge();
+    }
+
+    static void greet(String assistantName, String birthYear) {
+        System.out.println("Hello! My name is " + assistantName + ".");
+        System.out.println("I was created in " + birthYear + ".");
+        System.out.println("Please, remind me your name.");
+    }
+
+    static void remindName() {
+        String name = scanner.nextLine();
+        System.out.println("What a great name you have, " + name + "!");
+    }
+
+    static void guessAge() {
+        System.out.println("Let me guess your age.");
+        System.out.println("Say me remainders of dividing your age by 3, 5 and 7.");
+        int rem3 = scanner.nextInt();
+        int rem5 = scanner.nextInt();
+        int rem7 = scanner.nextInt();
+        int age = (rem3 * 70 + rem5 * 21 + rem7 * 15) % 105;
+        System.out.println("Your age is " + age + "; that's a good time to start programming!");
+    }
+}
